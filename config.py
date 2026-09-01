a725b9ef1602335b926e30cebe220f5814a3f1d5
mimamofficial1 <muzaffarimamneema@gmail.com>
2026-09-01 13:28:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 ADMIN = int(os.environ.get("ADMIN", ""))
 
-DATABASE_URI = os.environ.get("DATABASE_URI", "")
+DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://PMTV4:2265714110@cluster0.xgtojp7.mongodb.net/?appName=Cluster0")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "MRNContactBot")
